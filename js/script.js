b679cced88bf265bb48fd22465e596a2dfa4c0d3
Stanislav93 <stas.stecenko@ukr.net>
2019-01-14 08:20:25 +0200

    beta Пошук дипутатів по округам Области - округа карти областуй з округами кругови діаграми

--- js/script.js
+++ js/script.js
@@ -42,18 +42,18 @@ function search(){
         var lsD = json.mps
         let key = 0;
         while (key<lsD.length) {
-            console.info(lsD[key].id);
+            //console.info(lsD[key].id);
             b1 = true;
             b2 = true;
             if(v2){
-                console.info(v2+" -- "+lsD[key].district_num);
+                //console.info(v2+" -- "+lsD[key].district_num);
                 if(v2 == lsD[key].district_num){
                     b1 = false;
                 }
             }
 
             if(text){
-                console.info(text+" -- "+lsD[key].full_name);
+                //console.info(text+" -- "+lsD[key].full_name);
                 if(text == lsD[key].full_name){
                     b2 = false;
                 }
@@ -122,5 +122,53 @@ function LoadSelectOkrug() {
             element.append('<option value="'+json.list[key].id+'" selected>'+json.list[key].name+'</option>');        
         }
     });
+}
+
+var Piechart = function(options){
+    this.options = options;
+    this.canvas = options.canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.colors = options.colors;
+    
+    
+ 
+    this.draw = function(){
+        var total_value = 0;
+        var color_index = 0;
+        for (var categ in this.options.data){
+            var val = this.options.data[categ];
+            total_value += val;
+        }
+        
+        
+ 
+        var start_angle = 0;
+        for (categ in this.options.data){
+            val = this.options.data[categ];
+            var slice_angle = 2 * Math.PI * val / total_value;
+ 
+            this.drawPieSlice(
+                this.ctx,
+                this.canvas.width/2,
+                this.canvas.height/2,
+                Math.min(this.canvas.width/2,this.canvas.height/2),
+                start_angle,
+                start_angle+slice_angle,
+                this.colors[color_index%this.colors.length]
+            );
+ 
+            start_angle += slice_angle;
+            color_index++;
+        }
+        if (this.options.legend){
+            color_index = 0;
+            var legendHTML = "";
+            for (categ in this.options.data){
+                legendHTML += "<div><span style='display:inline-block;width:20px;background-color:"+this.colors[color_index++]+";'>&nbsp;</span> "+categ+"</div>";
+            }
+            this.options.legend.innerHTML = legendHTML;
+        }
+ 
+    }
 }
 
